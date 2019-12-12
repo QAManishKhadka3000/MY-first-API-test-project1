f6cb230506d3ce66304caca694707384708a0638
DELL <DELL@DESKTOP-B0ER32R>
2019-12-12 10:46:47 +0545

    First REST API Test

---
+++ Test Suites/REST API.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>REST API</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>82a09200-6182-4fe7-a414-2e3d40b7cd67</testSuiteGuid>
+   <testCaseLink>
+      <guid>cf47ab03-8c3c-447d-961d-567757c205cc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/REST API</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
